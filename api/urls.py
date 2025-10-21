6611fe778f4ccde1a5e55c059717ce936c061bd1
Sven Suneson <suneson96@gmail.com>
2025-10-21 08:46:23 +0200

    SCP-16 Add basic firebase authentication

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.getExample),
+    path('signup/', views.signupUser),
+    path('login/', views.loginUser),
+    path('refresh-token/', views.refreshToken),
 ]
