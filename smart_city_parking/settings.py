facfbb7420e8c2f10ff8f9bf6452253f60fe6b51
Sven Suneson <suneson96@gmail.com>
2025-10-29 10:24:26 +0100

    Fix read of .env

--- smart_city_parking/settings.py
+++ smart_city_parking/settings.py
@@ -10,25 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import environ
+import environ, os
 from pathlib import Path
 
-
+# Set casting and default values for environment variables
 env = environ.Env(
-    DJANGO_DEBUG=(bool, False),
+    DJANGO_DEBUG=(bool, True),
     DJANGO_SECRET_KEY=(str, 'your-default-secret-key'),
 )
 
-environ.Env.read_env()  # reads the .env file
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
