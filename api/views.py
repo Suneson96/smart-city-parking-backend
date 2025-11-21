3e047bd240256cf6b123bb20b135892ac356ca86
Suneson96 <69513635+Suneson96@users.noreply.github.com>
2025-11-21 14:46:34 +0100

    Update api/views.py
    Co-authored-by: Matteo Clementin <84046246+MateCle@users.noreply.github.com>

--- api/views.py
+++ api/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import requests
 from django.conf import settings
+from django.contrib.gis.geos import Point
 import firebase_admin
 from firebase_admin import auth as firebase_auth, credentials
 from . import models
