d3116a3c6706a410b520d4eedc9f6b0458deafc6
Sven Suneson <suneson96@gmail.com>
2025-11-25 11:56:10 +0100

    SCP-32 Remove unused import

--- api/views.py
+++ api/views.py
@@ -6,7 +6,6 @@ API views.
 # pylint: disable=broad-except
 
 import os
-from datetime import datetime
 from functools import wraps
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
